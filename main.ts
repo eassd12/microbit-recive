fafaa53906825e37673c6d242582da4e8760f759
eassd12 <124033183+eassd12@users.noreply.github.com>
2023-03-08 17:12:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+radio.onReceivedString(function (receivedString) {
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    led.plotBarGraph(
+    Math.map(signal, -95, -42, 0, 9),
+    9
+    )
+})
+let signal = 0
+radio.setGroup(4)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
